6b8c40845cf6595a3ea41578ccef0b34cc1f41c1
justinbyers <byersjustin3@gmail.com>
2019-05-09 18:40:06 -0400

    Expanded buttons functionality and content

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function hideCurrentInfo() {
     document.getElementById("home_Info").setAttribute("style", "visibility:hidden;");
     document.getElementById("event1_Info").setAttribute("style", "visibility:hidden;");
     document.getElementById("event2_Info").setAttribute("style", "visibility:hidden;");
+    document.getElementById("event3_Info").setAttribute("style", "visibility:hidden;");
+    document.getElementById("event4_Info").setAttribute("style", "visibility:hidden;");
+    document.getElementById("event5_Info").setAttribute("style", "visibility:hidden;");
+    document.getElementById("event6_Info").setAttribute("style", "visibility:hidden;");
+    document.getElementById("credits_Info").setAttribute("style", "visibility:hidden;");
+    
 }
 function showHome() {
     hideCurrentInfo();
@@ -15,4 +21,24 @@ function showEvent1() {
 function showEvent2() {
     hideCurrentInfo();
     document.getElementById("event2_Info").setAttribute("style", "visibility:visible;");
+}
+function showEvent3() {
+    hideCurrentInfo();
+    document.getElementById("event3_Info").setAttribute("style", "visibility:visible;");
+}
+function showEvent4() {
+    hideCurrentInfo();
+    document.getElementById("event4_Info").setAttribute("style", "visibility:visible;");
+}
+function showEvent5() {
+    hideCurrentInfo();
+    document.getElementById("event5_Info").setAttribute("style", "visibility:visible;");
+}
+function showEvent6() {
+    hideCurrentInfo();
+    document.getElementById("event6_Info").setAttribute("style", "visibility:visible;");
+}
+function showCredits() {
+    hideCurrentInfo();
+    document.getElementById("credits_Info").setAttribute("style", "visibility:visible;");
 }
